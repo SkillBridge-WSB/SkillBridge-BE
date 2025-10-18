a4260dfbe1387c77a711f013c4716551d2f620d8
Igor Nowakowski <igor.nowakowski@hawatel.com>
2025-10-18 13:19:07 +0200

    Initial commit

---
+++ src/test/java/pl/wsb/merito/skillbridge/SkillBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsb.merito.skillbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
