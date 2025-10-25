0ed984c8b3b4aa3274c36e8f87ed0528f172b18e
franek marciniak <prostyfranek@protonmail.com>
2025-10-25 16:39:14 +0200

    Fix register endpoint

--- src/main/java/pl/wsb/merito/skillbridge/rest/controller/AuthController.java
+++ src/main/java/pl/wsb/merito/skillbridge/rest/controller/AuthController.java
@@ -1,13 +1,17 @@
 package pl.wsb.merito.skillbridge.rest.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pl.wsb.merito.skillbridge.domain.service.auth.UserService;
 import pl.wsb.merito.skillbridge.rest.request.RegisterRequest;
 
@@ -16,11 +20,13 @@ import pl.wsb.merito.skillbridge.rest.request.RegisterRequest;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+    @PostMapping(path = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Map<String, String> register(@RequestBody RegisterRequest request) {
         userService.register(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Successfully registered");
+        return Map.of("message", "Successfully registered");
     }
 }
