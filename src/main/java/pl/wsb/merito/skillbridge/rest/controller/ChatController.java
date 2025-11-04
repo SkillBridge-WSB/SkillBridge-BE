c2e1fafabf65c1cf20d3796999fee2cb69c35b6e
wiktorbrzezinski <wiktor2002w@gmail.com>
2025-11-04 12:48:03 +0100

    domain aproach correciton

--- src/main/java/pl/wsb/merito/skillbridge/rest/controller/ChatController.java
+++ src/main/java/pl/wsb/merito/skillbridge/rest/controller/ChatController.java
@@ -5,14 +5,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
-import pl.wsb.merito.skillbridge.domain.model.Chat;
-import pl.wsb.merito.skillbridge.domain.model.Message;
 import pl.wsb.merito.skillbridge.domain.service.chat.ChatService;
 import pl.wsb.merito.skillbridge.rest.request.ChatMessageRequest;
 import pl.wsb.merito.skillbridge.rest.response.ChatMessageResponse;
+import pl.wsb.merito.skillbridge.rest.response.ChatResponse;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -24,21 +22,19 @@ public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
     public void sendMessage(@Payload ChatMessageRequest chatMessage) {
-        Chat chat = chatService.findChat(chatMessage.getChatId());
         ChatMessageResponse savedMsg = chatService.saveMessage(
-                chat.getId(), chatMessage.getSenderId(), chatMessage.getMessage()).toApiResponse();
+                chatMessage.getChatId(), chatMessage.getSenderId(), chatMessage.getMessage()).toApiResponse();
 
-        messagingTemplate.convertAndSend("/topic/chats/" + chat.getId(), savedMsg);
+        messagingTemplate.convertAndSend("/topic/chats/" + chatMessage.getChatId(), savedMsg);
     }
 
     @GetMapping("/find-chat")
-    public Map<String, UUID> findChat(@RequestParam UUID studentId, @RequestParam UUID tutorId) {
-        Chat chat = chatService.findOrCreateChat(studentId, tutorId);
-        return Map.of("chatId", chat.getId());
+    public ChatResponse findChat(@RequestParam UUID studentId, @RequestParam UUID tutorId) {
+        return chatService.findOrCreateChat(studentId, tutorId);
     }
 
     @GetMapping("/{chatId}/messages")
     public List<ChatMessageResponse> getMessages(@PathVariable UUID chatId) {
-        return chatService.getMessages(chatId).stream().map(Message::toApiResponse).toList();
+        return chatService.getMessages(chatId);
     }
 }
