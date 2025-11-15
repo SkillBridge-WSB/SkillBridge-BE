fa08ba45b2d48e41d64a64be2d42f9fdf8f85f8e
Igor Nowakowski <igor.nowakowski@hawatel.com>
2025-11-15 14:22:41 +0100

    ADD calendar endpoints

--- src/main/java/pl/wsb/merito/skillbridge/rest/request/Request.java
+++ src/main/java/pl/wsb/merito/skillbridge/rest/request/Request.java
@@ -1,6 +1,8 @@
 package pl.wsb.merito.skillbridge.rest.request;
 
 
+import java.time.Instant;
+
 public class Request {
     private Request() {
         // Private constructor to prevent instantiation
@@ -8,4 +10,5 @@ public class Request {
 
     public record UpdateUser(String name, String bio, String imageUrl) {}
     public record CreateSubject(String name, Integer costPerHour, String availability) {}
+    public record CreateCalendarSlot(Instant lessonTime) {}
 }
--- src/main/java/pl/wsb/merito/skillbridge/rest/response/Response.java
+++ src/main/java/pl/wsb/merito/skillbridge/rest/response/Response.java
@@ -2,6 +2,7 @@ package pl.wsb.merito.skillbridge.rest.response;
 
 import lombok.Builder;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class Response {
@@ -12,4 +13,5 @@ public class Response {
     @Builder
     public record User(UUID id, String email, String name, String bio, String imageUrl, String role) {}
     public record Subject(UUID id, String name, Integer costPerHour, String availability, UUID tutorId) {}
+    public record CalendarSlot(UUID id, Instant lessonTime, boolean available, UUID tutorId) {}
 }
