afaeb4a65199c04a18afc2e6aecdc6b4ea07a500
Franek Marciniak <prostyfranek@protonmail.com>
2025-12-01 11:11:58 +0100

    Move get tutors to use url param instead of body

--- src/main/java/pl/wsb/merito/skillbridge/rest/controller/TutorController.java
+++ src/main/java/pl/wsb/merito/skillbridge/rest/controller/TutorController.java
@@ -5,12 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wsb.merito.skillbridge.config.web.parameter.CurrentUserId;
 import pl.wsb.merito.skillbridge.domain.service.tutor.TutorService;
-import pl.wsb.merito.skillbridge.rest.request.Request;
 import pl.wsb.merito.skillbridge.rest.response.TutorListItemResponse;
 
 import java.util.List;
@@ -27,8 +26,8 @@ public class TutorController {
 
     @GetMapping("/all")
     public List<TutorListItemResponse> getTutorsForSubjects(@CurrentUserId UUID userId,
-                                                            @RequestBody Request.FindTutors req) {
+                                                            @RequestParam List<String> subjects) {
         log.debug("Fetching list of tutors");
-        return tutorService.findAllTutorsForSubjects(req.subjects(), userId);
+        return tutorService.findAllTutorsForSubjects(subjects, userId);
     }
 }
